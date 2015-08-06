2bd2d279de055e06925320e7c760b322f4516664
wathika <vcwathika@gmail.com>
2015-08-06 16:32:09 -0400

    added unslider

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*function get_email_link (link_title) {
+	var username = "jbrillon95";
+	var hostname = "gmail.com";
+	var link = username + "@" + hostname;
+	link_title = link_title || link;
+	document.write("<a href='" + "mail" + "to:" + link + "''>"+link_title + "</a>" )
+}*/
+
+
+$(document).ready(function(){
+	// Unslider jQuery
+	$('.banner').unslider();
+});

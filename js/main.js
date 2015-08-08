6b60db74283c03c91d68ac831c01a974fc09c13b
wathika <vcwathika@gmail.com>
2015-08-07 23:39:46 -0400

    intial commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
-/*function get_email_link (link_title) {
+function get_email_link (link_title) {
 	var username = "jbrillon95";
 	var hostname = "gmail.com";
 	var link = username + "@" + hostname;
 	link_title = link_title || link;
 	document.write("<a href='" + "mail" + "to:" + link + "''>"+link_title + "</a>" )
-}*/
-
-
-$(document).ready(function(){
-	// Unslider jQuery
-	$('.banner').unslider();
-});
+}
